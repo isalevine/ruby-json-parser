37a1a6962454445821dea53bee263a060b718176
isalevine <isa.loves.coding@gmail.com>
2019-09-09 23:12:58 -0700

    added ruby readout for json

--- parser.rb
+++ parser.rb
@@ -4,4 +4,28 @@ file = File.read("script.json")
 
 json = JSON.parse(file)
 
-puts json
+
+
+def json_readout(json)
+    scenario = json["scenarios"][0]
+
+    # better way to destructure, or access json with dot notation?
+    readout_str = <<-STR
+        Game: #{scenario["game"]}
+        Scenario Title: #{scenario["title"]}
+        Goal: #{scenario["goal"]}
+
+        Maps: #{scenario["maps"]}
+
+        Cells: #{scenario["cells"]}
+
+        Events: #{scenario["events"]}
+
+    STR
+
+    puts readout_str
+end
+
+
+
+json_readout(json)
