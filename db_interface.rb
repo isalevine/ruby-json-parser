7023749e83ddfd819c93ec1879bbd8cdb4744407
isalevine <isa.loves.coding@gmail.com>
2019-09-09 23:37:18 -0700

    experimenting with pg's json type

--- db_interface.rb
+++ db_interface.rb
@@ -4,3 +4,12 @@ require 'pg'
 
 conn = PG.connect(dbname: 'json_testdb')
 # conn.exec("CREATE DATABASE json_testdb")
+
+pg_str = <<-STR
+    CREATE TABLE json_scripts(
+        id serial PRIMARY KEY,
+        data json NOT NULL
+    );
+STR
+
+conn.exec(pg_str)
